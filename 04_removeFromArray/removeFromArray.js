f9db0997f352996d958ff62a581e70a9d83718ce
Jade Kim <csia19030@gmail.com>
2024-10-17 09:58:48 +0900

    solved exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    for(let i=0; i < toRemove.length; i++){
+        let indexToRemove = arr.indexOf(toRemove[i]);
+        while(indexToRemove != -1){
+            arr.splice(indexToRemove, 1);
+            indexToRemove = arr.indexOf(toRemove[i]);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
